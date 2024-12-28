24117d6c0458ba280efc0f976e4aa00ff6e8f942
Drxnzer <132266013+Drxnzer@users.noreply.github.com>
2024-12-28 15:19:33 +0530

    Update keep_alive.sh

--- keep_alive.sh
+++ keep_alive.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Keep the server running, executing aman.sh repeatedly and serving it on port 8080
 while true; do
     echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"
     ./aman.sh
-done | nc -l -p 8080 -q 1
+done | socat TCP-LISTEN:8080,reuseaddr,fork EXEC:./aman.sh
